9e08192c593edf179bf40d3f0b112e1130bddf00
being <34680324+being24@users.noreply.github.com>
2021-02-25 15:09:27 +0900

    update

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 docker stop sagumo
 docker rm sagumo
 docker pull ghcr.io/being24/sagumo:latest
-docker run -d -v sagumo-data:/opt --env-file .env --restart=always --name=sagumo ghcr.io/being24/sagumo
+docker run -d -v test-data:/opt/sagumo/data --env-file .env --restart=always --name=sagumo ghcr.io/being24/sagumo
