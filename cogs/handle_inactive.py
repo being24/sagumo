b59211d811b970985e0353ce604fe3dcd8b3791b
being24 <34680324+being24@users.noreply.github.com>
2021-08-12 15:41:48 +0900

    完成

--- cogs/handle_inactive.py
+++ cogs/handle_inactive.py
@@ -2,6 +2,7 @@
 
 import asyncio
 import logging
+import os
 import typing
 from datetime import datetime, timedelta
 
@@ -25,8 +26,10 @@ class InactiveDetector(commands.Cog):
         self.c = CommonUtil()
         self.inactive_mng = InactiveManager()
 
-        # self.inavtive_loop.stop()
-        # self.inavtive_loop.start()
+        self.inactive_loop.stop()
+        self.inactive_loop.start()
+
+        self.notify_msg_channel = os.getenv('NOTIFY_CHANNEL_ID')
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -38,6 +41,8 @@ class InactiveDetector(commands.Cog):
     @commands.command(aliases=['resi_active'],
                       description='指定された役職のメンバーをDBに登録するコマンド')
     async def register_roles_watching(self, ctx, *role_or_members: typing.Union[discord.Role, discord.Member]):
+        """指定された役職のメンバーをDBに登録するコマンド
+        """
         # 指定された役職とユーザーから、ユーザーのリストを作成する
         members = []
         for role_or_member in role_or_members:
@@ -46,47 +51,133 @@ class InactiveDetector(commands.Cog):
             else:
                 members.append(role_or_member)
 
+        # 重複除去
         members = list(set(members))
 
+        # IDの取り出し
+        members = [member.id for member in members]
+
         # DBにdo_nothingで登録する
-        print(members)
+        await self.inactive_mng.register_members(members)
 
-    async def check_inactive(self):
-        # 非アクティブを一覧する
-        pass
+        # 返事する
+        await ctx.reply(f'{len(members)}人のメンバーを登録しました')
 
-    async def check_active(self):
-        # アクティブを一覧する
-        pass
+    @commands.command(description='アクティブ化するコマンド')
+    async def activate(self, ctx, *users: discord.Member):
+        """指定されたメンバーをアクティブ化する
+        """
+        for user in users:
+            await self.inactive_mng.set_active(user.id)
+
+        await ctx.reply(f'{len(users)}人のメンバーをアクティブ化しました')
+
+    @commands.command(description='非アクティブ化するコマンド')
+    async def inactivate(self, ctx, *users: discord.Member):
+        """指定されたメンバーをアクティブ化する
+        """
+        for user in users:
+            await self.inactive_mng.set_inactive(user.id)
+
+        await ctx.reply(f'{len(users)}人のメンバーを非アクティブ化しました')
+
+    @commands.command(description='アクティブメンバーの一覧を表示する関数')
+    async def check_active(self, ctx):
+        """アクティブメンバーの一覧を表示する関数"""
+        actives = await self.inactive_mng.get_active_members()
+        if actives is None:
+            await ctx.reply('アクティブなメンバーはいません。そんなことある？')
+            return
+
+        embed = discord.Embed(title='アクティブメンバー一覧',
+                              description=f'{len(actives)}人のメンバーがアクティブです。')
+
+        for user_id in actives:
+            user = self.bot.get_user(user_id)
+            embed.add_field(name=user.name, value=user.mention)
+
+        await ctx.reply(embed=embed, allowed_mentions=discord.AllowedMentions.none())
+
+    @commands.command(description='非アクティブメンバーの一覧を表示する関数')
+    async def check_inactive(self, ctx):
+        """非アクティブメンバーの一覧を表示する関数"""
+        inactive = await self.inactive_mng.get_inactive_members()
+        if inactive is None:
+            await ctx.reply('非アクティブなメンバーはいません。')
+            return
+
+        embed = discord.Embed(title='非アクティブメンバー一覧',
+                              description=f'{len(inactive)}人のメンバーが非アクティブです。')
+
+        for user_id in inactive:
+            user = self.bot.get_user(user_id)
+            embed.add_field(name=user.name, value=user.mention)
+
+        await ctx.reply(embed=embed, allowed_mentions=discord.AllowedMentions.none())
 
     @commands.command(aliases=['remove_active'],
                       description='指定された役職のメンバーをDBから削除するコマンド')
     async def remove_member_watching(self, ctx, member: discord.Member):
-        # 指定されたメンバーをDBから削除する
-        pass
+        """指定されたメンバーをDBから削除するコマンド"""
+        # 指定されたメンバーのIDを取得
+        user_id = member.id
+        # DBに存在確認
+        if await self.inactive_mng.check_member(user_id):
+            await self.inactive_mng.remove_member(user_id)
+
+            # 返事する
+            await ctx.reply(f'{member.name}をDBから削除しました')
+        else:
+            await ctx.reply(f'{member.name}はDBに存在しません')
 
     @commands.Cog.listener()
     async def on_message(self, message):
         # メッセージの送り主がDBに登録されていた場合、last_postedを更新する
-        pass
+        if not await self.inactive_mng.check_member(message.author.id):
+            return
+        await self.inactive_mng.update_last_posted(message.author.id)
 
     @commands.Cog.listener()
-    async def on_raw_reaction_add(self, reaction: discord.Reaction, user: typing.Union[discord.Member, discord.User]):
+    async def on_raw_reaction_add(self, reaction: discord.RawReactionActionEvent):
         # リアクションの送り主がDBに登録されていた場合、last_reactを更新する
-
-        pass
+        if not await self.inactive_mng.check_member(reaction.user_id):
+            return
+        await self.inactive_mng.update_last_react(reaction.user_id)
 
     @commands.Cog.listener()
-    async def on_raw_reaction_remove(self, reaction: discord.Reaction, user: typing.Union[discord.Member, discord.User]):
+    async def on_raw_reaction_remove(self, reaction: discord.RawReactionActionEvent):
         # リアクションの削除主がDBに登録されていた場合、last_reactを更新する
+        if not await self.inactive_mng.check_member(reaction.user_id):
+            return
+        await self.inactive_mng.update_last_react(reaction.user_id)
 
-        pass
-
-    @tasks.loop(hours=1.0)
+    @tasks.loop(seconds=5.0)
     async def inactive_loop(self) -> None:
         # 一時間ごとにDBから非アクティブメンバーを検索して、通知する
-        # 最終のリアクションとポストの現在時間との差が3ヶ月以上で、通知されていない場合は通知する
-        pass
+        # 最終のリアクションとポストの現在時間との差が3ヶ月以上のアカウントを取得する
+        inactive_list = await self.inactive_mng.check_period_no_work()
+        if inactive_list is None:
+            return
+
+        # 埋込み型を作る
+        embed = discord.Embed(
+            title='非アクティブメンバー判定のメンバー一覧',
+            description=f'{len(inactive_list)}人のメンバーの活動が3ヶ月以上確認されていません。')
+
+        for user_id in inactive_list:
+            user = self.bot.get_user(user_id)
+            embed.add_field(name=user.name, value=user.mention)
+
+        # 送信先を取得する
+        channel = self.bot.get_channel(self.notify_msg_channel)
+        if channel is None:
+            channel = self.bot.get_channel(682930785480605760)
+
+        # 送信する
+        await channel.send(embed=embed, allowed_mentions=discord.AllowedMentions.none())
+
+        # 通知済みに設定する
+        await self.inactive_mng.set_notified(inactive_list)
 
     @inactive_loop.before_loop
     async def before_printer(self):
--- cogs/utils/inactive.py
+++ cogs/utils/inactive.py
@@ -5,14 +5,15 @@ import asyncio
 import dataclasses
 from dataclasses import dataclass
 from datetime import datetime
-from typing import List, Union
+from typing import List, Optional, Union
 
-from sqlalchemy import delete, select
+from dateutil.relativedelta import relativedelta
+from sqlalchemy import delete, or_, select, update
+from sqlalchemy.dialects.sqlite import insert
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import Column
-from sqlalchemy.sql.sqltypes import BOOLEAN, DATETIME
-from sqlalchemy.types import VARCHAR, BigInteger
+from sqlalchemy.types import BOOLEAN, DATETIME, VARCHAR, BigInteger
 
 try:
     from .db import engine
@@ -30,6 +31,7 @@ class InactiveDetector:
     user_id: int
     last_posted: datetime
     last_react: datetime
+    active: bool
     notified: bool
 
 
@@ -39,6 +41,7 @@ class InactiveDetectorDB(Base):
     user_id = Column(BigInteger, primary_key=True)  # ユーザーID
     last_posted = Column(DATETIME, nullable=False)  # 最終ポスト
     last_react = Column(DATETIME, nullable=False)  # 最終リアクション
+    active = Column(BOOLEAN, default=True)  # アクティブか？
     notified = Column(BOOLEAN, default=False)  # 通知済みか
 
 
@@ -58,22 +61,245 @@ class InactiveManager():
             user_id=db_data.user_id,
             last_posted=db_data.last_posted,
             last_react=db_data.last_react,
+            active=db_data.active,
             notified=db_data.notified)
         return db_data_raw
+
+    @staticmethod
+    def return_DBClass(data: InactiveDetector) -> InactiveDetectorDB:
+        """dataclassからDBのデータを返す関数
+
+        Args:
+            data (InactiveDetector): データクラス
+
+        Returns:
+            InactiveDetectorDB: DBのデータ
+        """
+        db_data = data
+        processed_data = InactiveDetectorDB(
+            user_id=db_data.user_id,
+            last_posted=db_data.last_posted,
+            last_react=db_data.last_react,
+            active=db_data.active,
+            notified=db_data.notified)
+
+        return processed_data
 
     async def create_table(self):
         async with engine.begin() as conn:
             await conn.run_sync(InactiveDetectorDB.metadata.create_all)
 
+    async def register_members(self, members: List[int]) -> None:
+        """メンバーを登録する関数
+
+        Args:
+            members (List[int]): リスト化されたメンバーID
+
+        Returns:
+            None
+        """
+        now = datetime.utcnow()
+
+        for member in members:
+            stmt = insert(InactiveDetectorDB).values(
+                user_id=member,
+                last_posted=now,
+                last_react=now,
+            )
+            do_nothing_stmt = stmt.on_conflict_do_nothing(
+                index_elements=['user_id'])
+
+            async with AsyncSession(engine) as session:
+                async with session.begin():
+                    await session.execute(do_nothing_stmt)
+
+    async def get_active_members(self) -> Optional[List[int]]:
+        """有効なメンバーを返す関数
+
+        Returns:
+            Optional[List[int]]: 有効なメンバーID
+        """
+        stmt = select([InactiveDetectorDB.user_id]).where(
+            InactiveDetectorDB.active)
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                result = await session.execute(stmt)
+                result = result.fetchall()
+                result = [member.user_id for member in result]
+
+        if len(result) == 0:
+            return None
+        else:
+            return result
+
+    async def get_inactive_members(self) -> Optional[List[int]]:
+        """非アクティブメンバーを返す関数
+
+        Returns:
+            Optional[List[int]]: 非アクティブなメンバーID
+        """
+        stmt = select([InactiveDetectorDB.user_id]).where(
+            InactiveDetectorDB.active == False)
+
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                result = await session.execute(stmt)
+                result = result.fetchall()
+                result = [member.user_id for member in result]
+
+        if len(result) == 0:
+            return None
+        else:
+            return result
+
+    async def get_all_members(self) -> Optional[List[int]]:
+        """全メンバーを返す関数
+
+        Returns:
+            Optional[List[int]]: 全メンバーID
+        """
+
+        stmt = select([InactiveDetectorDB.user_id])
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                result = await session.execute(stmt)
+                result = result.fetchall()
+                result = [member.user_id for member in result]
+
+        if len(result) == 0:
+            return None
+        else:
+            return result
+
+    async def check_member(self, member_id: int) -> bool:
+        stmt = select([InactiveDetectorDB.user_id]).where(
+            InactiveDetectorDB.user_id == member_id)
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                result = await session.execute(stmt)
+                result = result.fetchone()
+
+        if result is None:
+            return False
+        else:
+            return True
+
+    async def remove_member(self, member_id: int) -> None:
+        """メンバーを削除する関数
+
+        Args:
+            member_id (int): メンバーID
+        """
+
+        stmt = delete(InactiveDetectorDB).where(
+            InactiveDetectorDB.user_id == member_id)
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                await session.execute(stmt)
+
+    async def update_last_posted(self, member_id: int) -> None:
+        """最終投稿時間を更新する関数
+
+        Args:
+            member_id (int): メンバーID
+        """
+        stmt = update(InactiveDetectorDB).where(
+            InactiveDetectorDB.user_id == member_id).values(
+                last_posted=datetime.utcnow())
+
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                await session.execute(stmt)
+
+    async def update_last_react(self, member_id: int) -> None:
+        """最終リアクション時間を更新する関数
+
+        Args:
+            member_id (int): メンバーID
+        """
+        stmt = update(InactiveDetectorDB).where(
+            InactiveDetectorDB.user_id == member_id).values(
+            last_react=datetime.utcnow())
+
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                await session.execute(stmt)
+
+    async def check_period_no_work(self, month: int = 3) -> Optional[List[int]]:
+        """一定期間連続で投稿しなかったメンバーを返す関数
+
+        Args:
+            month (int, optional): 閾値期間. Defaults to 3.
+
+        Returns:
+            Optional[List[int]]: 一定期間連続で投稿しなかったメンバーIDのリスト
+        """
+        now = datetime.utcnow()
+        month_ago = now - relativedelta(months=month)
+        stmt = select([InactiveDetectorDB.user_id]).where(InactiveDetectorDB.notified == False).filter(
+            or_(InactiveDetectorDB.last_posted < month_ago, InactiveDetectorDB.last_react < month_ago))
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                result = await session.execute(stmt)
+                result = result.fetchall()
+                result = [member.user_id for member in result]
+
+        if len(result) == 0:
+            return None
+        else:
+            return result
+
+    async def set_notified(self, member_list: List[int]) -> None:
+        """通知済み、非アクティブに設定する関数
+
+        Args:
+            member_list (List[int]): 一定期間連続で投稿しなかったメンバーIDのリスト
+        """
+        for member in member_list:
+            stmt = update(InactiveDetectorDB).where(
+                InactiveDetectorDB.user_id == member).values(
+                active=False, notified=True)
+            async with AsyncSession(engine) as session:
+                async with session.begin():
+                    await session.execute(stmt)
+
+    async def set_active(self, member_id: int) -> None:
+        """メンバーをアクティブに設定する関数
+
+        Args:
+            member_id (int): アクティブにするメンバーID
+        """
+        now = datetime.utcnow()
+        stmt = update(InactiveDetectorDB).where(
+            InactiveDetectorDB.user_id == member_id).values(
+            last_posted=now, last_react=now, active=True, notified=False)
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                await session.execute(stmt)
+
+    async def set_inactive(self, member_id: int) -> None:
+        """メンバーを非アクティブに設定する関数
+
+        Args:
+            member_id (int): 非アクティブにするメンバーID
+        """
+        stmt = update(InactiveDetectorDB).where(
+            InactiveDetectorDB.user_id == member_id).values(
+            active=False, notified=True)
+        async with AsyncSession(engine) as session:
+            async with session.begin():
+                await session.execute(stmt)
+
 
 if __name__ == "__main__":
-    polling_mng = InactiveManager()
-    result = asyncio.run(polling_mng.create_table())
+    invite_mng = InactiveManager()
+    # result = asyncio.run(invite_mng.create_table())
 
     now = datetime.now()
 
     # test = InactiveDetector(message_id=124, author_id=123, created_at=now)
-    print(asyncio.run(polling_mng.get_aggregation(814348307827523585)))
+    result = asyncio.run(invite_mng.check_period_no_work())
+    print(result)
 
     # for i in result:
     #    print(i)
