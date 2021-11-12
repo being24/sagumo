1093213a34d0b096b740d0882a07ee5ee0124d37
being24 <34680324+being24@users.noreply.github.com>
2021-11-12 19:29:19 +0900

    🔥 不必要なコードを除去

--- cogs/handle_inactive.py
+++ cogs/handle_inactive.py
@@ -130,20 +130,6 @@ class InactiveDetector(commands.Cog):
         else:
             await ctx.reply(f'{member.name}はDBに存在しません')
 
-    @commands.command(description='指定された役職のメンバーをDBから削除するコマンド')
-    async def test(self, ctx):
-        channel = self.bot.get_channel(self.notify_msg_channel)
-        if channel is None:
-            channel = self.bot.get_channel(682930390276505601)
-
-        # 送信する
-        try:
-            await channel.send('embed=embed', allowed_mentions=discord.AllowedMentions.none())
-        except discord.Forbidden:
-            channel = self.bot.get_channel(609059625098018828)
-            await channel.send('embed=embed', allowed_mentions=discord.AllowedMentions.none())
-
-
     @commands.Cog.listener()
     async def on_message(self, message):
         # メッセージの送り主がDBに登録されていた場合、last_postedを更新する
